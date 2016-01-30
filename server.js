7c10c532fcc78ecb62a9ed17542f3c83514fc192
Noam Gal <atgardner@gmail.com>
2016-01-30 22:56:55 +0200

    trying to figure out the right port

--- server.js
+++ server.js
@@ -33,8 +33,8 @@ app.get('/', function (req, res) {
     res.send('Hello World from express!');
 });
 
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
+app.listen(process.env.PORT || 3000, function () {
+    console.log(`Example app listening on port ${process.env.PORT || 3000}!`);
 });
 
 //console.log('before init');
