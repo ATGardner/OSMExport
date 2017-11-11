9e6c67e1bdf942cb9abb4d47006412d5255dea64
Noam Gal <atgardner@gmail.com>
2017-11-11 18:49:54 +0200

    removed old wrapper classes fixed the markers and reverse options

--- src/osm/osmWrapper.js
+++ src/osm/osmWrapper.js
@@ -1,33 +1,12 @@
 'use strict';
-const moment = require('moment');
 const winston = require('winston');
 const osmApi = require('./osmApi');
-const Node = require('./Node');
-const Relation = require('./Relation');
 
-function getSubRelations({subRelationIds}) {
-  const promises = subRelationIds.map(getFullRelation);
-  return Promise.all(promises);
-}
-
-async function getFullRelation(relationId) {
+function getFullRelation(relationId) {
   winston.verbose(`Getting full relation '${relationId}'`);
-  const {elements} = await osmApi.fetchRelation(relationId, true);
-  const relations = elements.filter(e => e.type === 'relation');
-  const ways = elements.filter(e => e.type === 'way');
-  const nodes = elements.filter(e => e.type === 'node');
-  const relation = new Relation(relations, ways, nodes);
-  relation.subRelations = await getSubRelations(relation);
-  return relation;
-}
-
-async function getWater(relationId) {
-  winston.verbose(`Getting relation water for ${relationId}`);
-  const {elements} = await osmApi.fetchWater(relationId, 1000);
-  return elements.map(e => new Node(e));
+  return osmApi.fetchRelation(relationId, true);
 }
 
 module.exports = {
   getFullRelation,
-  getWater
 };
