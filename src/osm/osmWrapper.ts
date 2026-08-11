cdedfaf8ee26d49df47189543ee0c78b417bb90b
noam gal <atgardner@gmail.com>
2026-08-11 15:50:03 +0300

    feat: add TypeScript implementation for osmWrapper module

--- src/osm/osmWrapper.ts
+++ src/osm/osmWrapper.ts
@@ -1,12 +1,11 @@
-'use strict';
-
-import {fetchNodesInRelation, fetchRelation} from './osmApi.mjs';
-import {getLogger} from '../logger.mjs';
+import { fetchNodesInRelation, fetchRelation } from './osmApi.ts';
+import { getLogger } from '../logger.ts';
 import osmtogeojson from 'osmtogeojson';
+import type { FeatureCollection, GeometryObject } from 'geojson';
 
 const logger = getLogger('osmWrapper');
 
-export async function getFullRelation(relationId, filter = true) {
+export async function getFullRelation(relationId: string, filter = true): Promise<FeatureCollection<GeometryObject>> {
   logger.verbose(`Getting full relation '${relationId}'`);
   const osmJson = await fetchRelation(relationId);
   return osmtogeojson(osmJson, {
@@ -16,7 +15,7 @@ export async function getFullRelation(relationId, filter = true) {
   });
 }
 
-export async function getRelationNodes(relationId) {
+export async function getRelationNodes(relationId: string): Promise<FeatureCollection<GeometryObject>> {
   logger.verbose(`Getting nodes for relation '${relationId}'`);
   const osmJson = await fetchNodesInRelation(relationId);
   return osmtogeojson(osmJson, {
